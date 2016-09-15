4d89180181a08b67b2b2a9532b6d660196c9408f
Connor M <connormitchell80@gmail.com>
2016-09-15 11:45:34 -0500

    moded 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,6 @@
 from __future__ import division
 from decimal import Decimal
 
-game_number = [0,1,2,3,4,5]
 probability = float([0.5,0.4,0.6,0.75,0.3,0.83])_
 
 nogames = float(input("Enter number of games "))
@@ -12,9 +11,21 @@ else:
     userlist = []
     maxLength = 6
     while len(userlist) < maxLength:
+        x = 0
         useradd = input("Enter Game Number ")
-        userlist.append(useradd)
+        if useradd = 1:
+            userlist.append(0.4)
+        elif useradd = 2:
+            userlist.append(0.6)
+        elif useradd = 3:
+            userlist.append(0.75)
+        elif useradd = 4
+            userlist.append(0.3)
+        elif useradd = 5:
+            userlist.append(0.83)
         again = float(raw_input("Do you want to add another number?(y/n) "))
         if again == "n":
             maxLength = 0
-        
+        else:
+            x + 1
+            
