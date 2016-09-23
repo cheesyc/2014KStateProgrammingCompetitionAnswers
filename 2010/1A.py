ef45ab9adde0f697ef04bddfed1f0da16feadc22
Connor Mitchell <connormitchell80@gmail.com>
2016-09-23 13:10:37 -0500

    wip

---
+++ 2010/1A.py
@@ -0,0 +1,227 @@
+gas = float(input("Cost of gas: "))
+numcars = float(input("Amount of cars: "))
+
+constant = 15/gas
+
+if numcars == 1:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+    calc1 = ((car1miles/car1gas) - constant)/car1cost
+    print ("Car 1 is the best car. Years to pay off:", calc1 )
+elif numcars == 2:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+elif numcars == 3:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+elif numcars == 4:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+elif numcars == 5:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+
+    car5cost = float(input("Cost of car 5: "))
+    car5miles = float(input("Amount of miles on car 5: "))
+    car5gas = float(input("Gallons of gas on car 5: "))
+elif numcars == 6:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+
+    car5cost = float(input("Cost of car 5: "))
+    car5miles = float(input("Amount of miles on car 5: "))
+    car5gas = float(input("Gallons of gas on car 5: "))
+
+    car6cost = float(input("Cost of car 6: "))
+    car6miles = float(input("Amount of miles on car 6: "))
+    car6gas = float(input("Gallons of gas on car 6: "))
+elif numcars == 7:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+
+    car5cost = float(input("Cost of car 5: "))
+    car5miles = float(input("Amount of miles on car 5: "))
+    car5gas = float(input("Gallons of gas on car 5: "))
+
+    car6cost = float(input("Cost of car 6: "))
+    car6miles = float(input("Amount of miles on car 6: "))
+    car6gas = float(input("Gallons of gas on car 6: "))
+
+    car7cost = float(input("Cost of car 7: "))
+    car7miles = float(input("Amount of miles on car 7: "))
+    car7gas = float(input("Gallons of gas on car 7: "))
+elif numcars == 8:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+
+    car5cost = float(input("Cost of car 5: "))
+    car5miles = float(input("Amount of miles on car 5: "))
+    car5gas = float(input("Gallons of gas on car 5: "))
+
+    car6cost = float(input("Cost of car 6: "))
+    car6miles = float(input("Amount of miles on car 6: "))
+    car6gas = float(input("Gallons of gas on car 6: "))
+
+    car7cost = float(input("Cost of car 7: "))
+    car7miles = float(input("Amount of miles on car 7: "))
+    car7gas = float(input("Gallons of gas on car 7: "))
+
+    car8cost = float(input("Cost of car 8: "))
+    car8miles = float(input("Amount of miles on car 8: "))
+    car8gas = float(input("Gallons of gas on car 8: "))
+elif numcars == 9:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+
+    car5cost = float(input("Cost of car 5: "))
+    car5miles = float(input("Amount of miles on car 5: "))
+    car5gas = float(input("Gallons of gas on car 5: "))
+
+    car6cost = float(input("Cost of car 6: "))
+    car6miles = float(input("Amount of miles on car 6: "))
+    car6gas = float(input("Gallons of gas on car 6: "))
+
+    car7cost = float(input("Cost of car 7: "))
+    car7miles = float(input("Amount of miles on car 7: "))
+    car7gas = float(input("Gallons of gas on car 7: "))
+
+    car8cost = float(input("Cost of car 8: "))
+    car8miles = float(input("Amount of miles on car 8: "))
+    car8gas = float(input("Gallons of gas on car 8: "))
+
+    car9cost = float(input("Cost of car 9: "))
+    car9miles = float(input("Amount of miles on car 9: "))
+    car9gas = float(input("Gallons of gas on car 9: "))
+elif numcars == 10:
+    car1cost = float(input("Cost of car 1: "))
+    car1miles = float(input("Amount of miles on car 1: "))
+    car1gas = float(input("Gallons of gas on car 1: "))
+
+    car2cost = float(input("Cost of car 2: "))
+    car2miles = float(input("Amount of miles on car 2: "))
+    car2gas = float(input("Gallons of gas on car 2: "))
+
+    car3cost = float(input("Cost of car 3: "))
+    car3miles = float(input("Amount of miles on car 3: "))
+    car3gas = float(input("Gallons of gas on car 3: "))
+
+    car4cost = float(input("Cost of car 4: "))
+    car4miles = float(input("Amount of miles on car 4: "))
+    car4gas = float(input("Gallons of gas on car 4: "))
+
+    car5cost = float(input("Cost of car 5: "))
+    car5miles = float(input("Amount of miles on car 5: "))
+    car5gas = float(input("Gallons of gas on car 5: "))
+
+    car6cost = float(input("Cost of car 6: "))
+    car6miles = float(input("Amount of miles on car 6: "))
+    car6gas = float(input("Gallons of gas on car 6: "))
+
+    car7cost = float(input("Cost of car 7: "))
+    car7miles = float(input("Amount of miles on car 7: "))
+    car7gas = float(input("Gallons of gas on car 7: "))
+
+    car8cost = float(input("Cost of car 8: "))
+    car8miles = float(input("Amount of miles on car 8: "))
+    car8gas = float(input("Gallons of gas on car 8: "))
+
+    car9cost = float(input("Cost of car 9: "))
+    car9miles = float(input("Amount of miles on car 9: "))
+    car9gas = float(input("Gallons of gas on car 9: "))
+
+    car10cost = float(input("Cost of car 10: "))
+    car10miles = float(input("Amount of miles on car 10: "))
+    car10gas = float(input("Gallons of gas on car 10: "))
